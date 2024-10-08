e0eddaf8fdb7cc39404c9ee4d1b85e64087f7b9b
Tulasi Naga Sai <btnagasai@gmail.com>
2024-10-08 17:32:58 +0530

    srcchange

--- src/Gridtemplate.jsx
+++ src/Gridtemplate.jsx
@@ -5,7 +5,7 @@ const Gridtemplate = () =>{
             {/* first grid */}
               <div className="bg-[rgb(250,238,226)] text-balance text-[40px]  font-bold row-span-2 rounded-2xl p-5 " >Create and schedule content
                <div className="text-[#7651dc]">quicker</div>
-               <img src="src/assets/illustration-create-post.webp" alt="create button" />
+               <img src="/assets/illustration-create-post.webp" alt="create button" />
                </div> 
                  {/* second grid */}
               <div className="bg-[#7651dc] col-span-2  text-wrap font-bold  text-white text-[60px] text-center rounded-2xl p-16">Social Media 
@@ -15,25 +15,25 @@ const Gridtemplate = () =>{
                 </div>
                   {/* third grid */}
               <div className="bg-[#dcd1fc] row-span-3 text-left font-extrabold text-[50px] p-5 overflow-hidden rounded-2xl">Schedule to social media
-              <img src="src/assets/3rdimage.png" alt="grapline" className=" w-96 h-96 ml-[90px] " />
+              <img src="/assets/3rdimage.png" alt="grapline" className=" w-96 h-96 ml-[90px] " />
               <div className="text-[25px] font-normal pt-5">optimize not timings to content publish </div>
               </div>   
                 {/* fourth grid */}          
                
                 <div className="bg-white text-balance text-[40px] text-left font-bold grid gap-2 row-span-2 overflow-hidden  p-2 rounded-2xl" >
-                    <img src="src/assets/illustration-multiple-platforms.webp" alt="insta twitter id's" className="ml-16  h-16 w-auto"/>
+                    <img src="/assets/illustration-multiple-platforms.webp" alt="insta twitter id's" className="ml-16  h-16 w-auto"/>
                     Manage multiple accounts and platforms.</div>
                 {/* fifth grid */} 
                 <div className="bg-[#ffcc69] text-balance text-xl overflow-hidden text-[40px] ml-16 font-bold leading-[40px] text-left pt-16 font-boldleading-[35px] grid items-end pl-2 pr-0 row-span-2 rounded-2xl">Maintain a consistent posting schedule
-                <img src="src/assets/sixth.png" alt="" className=" w-60 ml-5"/></div>
+                <img src="/assets/sixth.png" alt="" className=" w-60 ml-5"/></div>
                     {/* sixth grid */}
                
                 <div className="bg-[#ffcc69] text-balance text-left text-[35px] font-bold row-span-2 p-5 grid gap-5 rounded-2xl">Write your content using AI.
-                <img src="src/assets/fourth.png" alt="user chat"  className="w-56 h-56 ml-[25px]  "/>
+                <img src="/assets/fourth.png" alt="user chat"  className="w-56 h-56 ml-[25px]  "/>
                 </div>
                 {/* seven grid */}
-                <div className="bg-white text-balance  text-center rounded-2xl "><span className="text-[55px] text-center font-bold">&#62;56%</span><br/> footer audience growth <img src="src/assets/illustration-audience-growth.webp" alt="" className="w-[280px] h-[124px] ml-10"/> </div>
-                <div className="bg-[#7651dc] col-span-2 p-4  text-[30px]  font-bold text-white rounded-2xl"><span className="grid grid-cols-2 items-center justify-center "><img src="src/assets/illustration-grow-followers.webp" alt="" className="w-[250px] h-[200px]" />
+                <div className="bg-white text-balance  text-center rounded-2xl "><span className="text-[55px] text-center font-bold">&#62;56%</span><br/> footer audience growth <img src="/assets/illustration-audience-growth.webp" alt="" className="w-[280px] h-[124px] ml-10"/> </div>
+                <div className="bg-[#7651dc] col-span-2 p-4  text-[30px]  font-bold text-white rounded-2xl"><span className="grid grid-cols-2 items-center justify-center "><img src="/assets/illustration-grow-followers.webp" alt="" className="w-[250px] h-[200px]" />
                     Grow followers with non-stop content.</span>
                 
                 </div>
