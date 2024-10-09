c9262675e87851536508ab5cd436f80a93f0eccd
Tulasi Naga Sai <btnagasai@gmail.com>
2024-10-09 13:14:54 +0530

    fontfamily

--- src/Gridtemplate.jsx
+++ src/Gridtemplate.jsx
@@ -3,37 +3,40 @@ const Gridtemplate = () =>{
         <div className="flex justify-center items-center h-screen max-w-screen-2xl m-auto p-5 bg-[#f5f5f5]">
         <div className="grid grid-cols-4  gap-4  ">
             {/* first grid */}
-              <div className="bg-[rgb(250,238,226)] text-balance text-[40px]  font-bold row-span-2 rounded-2xl p-5 " >Create and schedule content
-               <div className="text-[#7651dc]">quicker</div>
+              <div className="bg-[rgb(250,238,226)]  text-[45px] font-dm font-bold row-span-2 rounded-2xl p-5 " ><div>Create and schedule content</div>
+              <i className="text-[#7651dc] ">quicker</i>
                <img src="/assets/illustration-create-post.webp" alt="create button" />
                </div> 
                  {/* second grid */}
-              <div className="bg-[#7651dc] col-span-2  text-wrap font-bold  text-white text-[60px] text-center rounded-2xl p-16">Social Media 
-                <span className="text-[#ffcc69]">10x</span> Faster with AI
+              <div className="bg-[#7651dc] col-span-2  text-wrap font-bold font-dm  text-white text-[65px] text-center rounded-2xl p-16">Social Media 
+                <span className="text-[#ffcc69] font-dm">10x</span> Faster with AI
                 <div className="text-[#ffcc69] text-lg ">⭐⭐⭐⭐⭐ </div>
-                <div className="text-lg font-medium"> Over 4,000 5-star reviews</div>
+                <div className="text-lg font-medium font-dm"> Over 4,000 5-star reviews</div>
                 </div>
                   {/* third grid */}
-              <div className="bg-[#dcd1fc] row-span-3 text-left font-extrabold text-[50px] p-5 overflow-hidden rounded-2xl">Schedule to social media
+              <div className="bg-[#dcd1fc] row-span-3 text-left font-extrabold text-[50px] font-dm p-5 overflow-hidden rounded-2xl">Schedule to social media
               <img src="/assets/3rdimage.png" alt="grapline" className=" w-96 h-96 ml-[90px] " />
-              <div className="text-[25px] font-normal pt-5">optimize not timings to content publish </div>
+              <div className="text-[30px] font-dm font-semibold  pt-5">optimize not timings to content publish </div>
               </div>   
                 {/* fourth grid */}          
                
-                <div className="bg-white text-balance text-[40px] text-left font-bold grid gap-2 row-span-2 overflow-hidden  p-2 rounded-2xl" >
+                <div className="bg-white text-balance text-[40px] text-left font-bold font-dm grid gap-2 row-span-2 overflow-hidden  p-2 rounded-2xl" >
                     <img src="/assets/illustration-multiple-platforms.webp" alt="insta twitter id's" className="ml-16  h-16 w-auto"/>
                     Manage multiple accounts and platforms.</div>
                 {/* fifth grid */} 
-                <div className="bg-[#ffcc69] text-balance text-xl overflow-hidden text-[40px] ml-16 font-bold leading-[40px] text-left pt-16 font-boldleading-[35px] grid items-end pl-2 pr-0 row-span-2 rounded-2xl">Maintain a consistent posting schedule
+                <div className="bg-[#ffcc69] text-balance  overflow-hidden text-[48px]  font-dm leading-[50px] text-left pt-10 font-bold gap-4 grid items-end pl-2 pr-0 row-span-2 rounded-2xl">Maintain a consistent posting schedule
                 <img src="/assets/sixth.png" alt="" className=" w-60 ml-5"/></div>
                     {/* sixth grid */}
                
-                <div className="bg-[#ffcc69] text-balance text-left text-[35px] font-bold row-span-2 p-5 grid gap-5 rounded-2xl">Write your content using AI.
+                <div className="bg-[#ffcc69] text-balance font-dm text-left text-[45px] font-bold row-span-2 p-5 grid gap-5 rounded-2xl">Write your content using AI.
                 <img src="/assets/fourth.png" alt="user chat"  className="w-56 h-56 ml-[25px]  "/>
                 </div>
                 {/* seven grid */}
-                <div className="bg-white text-balance  text-center rounded-2xl "><span className="text-[55px] text-center font-bold">&#62;56%</span><br/> footer audience growth <img src="/assets/illustration-audience-growth.webp" alt="" className="w-[280px] h-[124px] ml-10"/> </div>
-                <div className="bg-[#7651dc] col-span-2 p-4  text-[30px]  font-bold text-white rounded-2xl"><span className="grid grid-cols-2 items-center justify-center "><img src="/assets/illustration-grow-followers.webp" alt="" className="w-[250px] h-[200px]" />
+                <div className="bg-white text-balance font-dm text-center rounded-2xl "><span className="text-[55px] text-center font-bold">&#62;56%</span><br/> footer audience growth <img src="/assets/illustration-audience-growth.webp" alt="" className="w-[280px] h-[124px] ml-10"/> </div>
+               
+                <div className="bg-[#7651dc] col-span-2 font-dm p-4  text-[40px]  font-bold text-white rounded-2xl">
+                    <span className="grid grid-cols-2 items-center justify-center font-dm  ">
+                        <img src="/assets/illustration-grow-followers.webp" alt="" className="w-[250px] h-[200px]" />
                     Grow followers with non-stop content.</span>
                 
                 </div>
