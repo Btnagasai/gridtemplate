e437b3b621f28041b80545ba50606cd119ee2ffc
Tulasi Naga Sai <btnagasai@gmail.com>
2024-10-09 15:16:06 +0530

    gridtemplatechange

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+
 import Gridtemplate from './Gridtemplate'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     
       <div>
         <Gridtemplate/>
+        
       </div>
    
   )
